69c00b328bf1f5a24d530662fb6e3b1017b375fb
Przemo <przemo06133@gmail.com>
2020-11-16 21:32:14 +0100

    get all items

--- src/handlers/getItem.js
+++ src/handlers/getItem.js
@@ -1,14 +1,31 @@
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.itemTableName;
+
 exports.handler = async (event) => {
-	console.log('event ', event);
-	return {
-		statusCode: 200,
-		body: JSON.stringify(
-			{
-				message: 'Go Serverless v1.0! Your function executed successfully!',
-				input: event,
-			},
-			null,
-			2
-		),
-	};
+	try {
+		const itemId = event.pathParameters.id;
+		let result;
+		if (itemId === 'all') {
+			result = await documentClient.scan({ TableName: tableName }).promise();
+		} else {
+			const params = {
+				TableName: tableName,
+				Key: {
+					itemId,
+				},
+			};
+			result = await documentClient.get(params).promise();
+		}
+		console.log('result ', result);
+		return {
+			statusCode: 200,
+			body: JSON.stringify(result, null, 2),
+		};
+	} catch (error) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify(error),
+		};
+	}
 };
